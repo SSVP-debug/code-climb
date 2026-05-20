bc687eb8176dadf8c0227a18f799d3b9e14adc79
SSVP-debug <ssvp006@gmail.com>
2026-05-21 00:20:20 +0530

    Standard version

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import Dashboard from "./pages/Dashboard";
+import Analytics from "./pages/Analytics";
+import Profile from "./pages/Profile";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/analytics" element={<Analytics />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  );
+}
+
+export default App;
