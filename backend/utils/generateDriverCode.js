fbb4cb23e63f7015d0a35705f487a6e82d1f3e26
SSVP-debug <ssvp006@gmail.com>
2026-06-24 09:28:59 +0530

    GenerateDriverCode update in C++

--- backend/utils/generateDriverCode.js
+++ backend/utils/generateDriverCode.js
@@ -204,9 +204,7 @@ class Main {
     const cppCallArgs = args.map((a) => a.key).join(", ");
 
     const generated = `
-#include <iostream>
-#include <vector>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
 
 ${userCode}
